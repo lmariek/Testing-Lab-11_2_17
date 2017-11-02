2880a8727683c260eb945fad1e5e7fba90a71f6b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-11-02 15:14:09 -0700

    Done.

---
+++ tests.py
@@ -0,0 +1,50 @@
+"""Tests for Balloonicorn's Flask app."""
+
+import unittest
+import party
+
+
+class PartyTests(unittest.TestCase):
+    """Tests for my party site."""
+
+    def setUp(self):
+        """Code to run before every test."""
+
+        self.client = party.app.test_client()
+        party.app.config['TESTING'] = True
+
+    def test_homepage(self):
+        """Can we reach the homepage?"""
+
+        result = self.client.get("/")
+        self.assertIn("having a party", result.data)
+
+    def test_no_rsvp_yet(self):
+        """Do users who haven't RSVPed see the correct view?"""
+
+        result = self.client.get("/")
+        self.assertNotIn("Party Details", result.data)
+
+    def test_rsvp(self):
+        """Do RSVPed users see the correct view?"""
+
+        rsvp_info = {'name': "Jane", 'email': "jane@jane.com"}
+
+        result = self.client.post("/rsvp", data=rsvp_info,
+                                  follow_redirects=True)
+
+        self.assertIn("Party Details", result.data)
+        self.assertNotIn("Please RSVP", result.data)
+
+    def test_rsvp_mel(self):
+        """Can we keep Mel out?"""
+
+        rsvp_info = {'name': "Mel Melitpolski", 'email': "mel@ubermelon.com"}
+
+        result = self.client.post("/rsvp", data=rsvp_info,
+                                  follow_redirects=True)
+        self.assertNotIn("Party Details", result.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
